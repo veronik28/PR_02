79512c84c5ef2a8184831466c900a67afe476c8e
Veronika <157686128+veronik28@users.noreply.github.com>
2025-02-03 20:24:57 +0300

    Add files via upload

---
+++ WebApplication/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using WebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(
+    DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Producer> Producer { get; set; }
+
+    }
+}
---
+++ WebApplication/Migrations/20250127124939_AddFieldDescription.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFieldDescription : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Producer",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Producer",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Rating",
+                table: "Producer",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Producer");
+
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Producer");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Producer",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
